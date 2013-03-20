d6c3f0dbf48e969cf56691fee5c92c0c492885e4
Matthew J Swann <mjs0031@auburn.edu>
2013-03-20 17:29:36 -0500

    fixturized json testdata

--- Control/admin.py
+++ Control/admin.py
@@ -2,7 +2,7 @@
 # Not Applicable
 
 """ Django Package Imports """
-# Not Applicable
+from django.contrib import admin
 
 """     Internal Package Imports    """
 """ ------------------------------- """
@@ -19,3 +19,62 @@
  Code for the Django admin site.
  
  """
+
+class BusinessAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'name', 'city', 'state', 'phone')
+    list_filter   = ('state',)
+    search_fields = ['name', 'city', 'state']
+    ordering      = ['state', 'city']
+    fieldsets     = (               
+        ( 'Advanced options', {
+            'classes': ('wide', 'extrapretty'),
+            'fields' : ('name', 'address_line_one', 'address_line_two',
+                        'city', 'state', 'zip_code', 'phone',
+                        'notes', 'business_description')
+                 }),)
+    
+class PersonAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'last_name', 'first_name', 'state', 'business')
+    list_filter   = ('state',)
+    search_fields = ['last_name', 'city', 'state']
+    ordering      = ['state', 'city', 'last_name']
+    fieldsets     = (               
+        ( 'Advanced options', {
+            'classes': ('wide', 'extrapretty'),
+            'fields' : ('last_name', 'first_name', 'business', 'address_line_one', 'address_line_two',
+                        'city', 'state', 'zip_code', 'date_registered')
+                 }),)
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'name', 'city', 'state')
+    list_filter   = ('state',)
+    search_fields = ['name', 'city', 'state']
+    ordering      = ['state', 'city']
+    fieldsets     = (               
+        ( 'Advanced options', {
+            'classes': ('wide', 'extrapretty'),
+            'fields' : ('name', 'address_line_one', 'address_line_two',
+                        'city', 'state', 'zip_code', 'phone',
+                        'notes', 'education_level')
+                 }),)
+    
+class ChildAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'last_name', 'first_name', 'school')
+    search_fields = ['last_name',]
+    ordering      = ['last_name']
+    fieldsets     = (               
+        ( 'Advanced options', {
+            'classes': ('wide', 'extrapretty'),
+            'fields' : ('last_name', 'first_name', 'school', 'family')
+                 }),)
+
+from Data_Base.models import (
+    Business,
+    School,
+    Person,
+    Child                     
+        )
+admin.site.register(Business, BusinessAdmin)
+admin.site.register(School, SchoolAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Child, ChildAdmin)
